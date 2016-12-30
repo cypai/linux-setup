cb0d508b77b39d720925a3d5ef66822c49d7c194
Cheng-Yu Pai <mail4charliepai@gmail.com>
2016-12-30 00:00:06 -0800

    Added vim configuration

--- setup.sh
+++ setup.sh
@@ -1,29 +1,39 @@
 #!/usr/bin/env bash
 
+CURR_DIR=`pwd`
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR
 
 echo "Initializing temp directory..."
-if [ -d $SCRIPT_DIR/temp ]; then
-	rm -rf $SCRIPT_DIR/temp
+if [ -d temp ]; then
+    rm -rf temp
 fi
-mkdir $SCRIPT_DIR/temp
+mkdir temp
 
 PACKAGES="git gitk vim xclip openjdk-8-jdk maven gradle ant python-pip python3-pip mc ncdu vlc"
 echo "Installing packages: $PACKAGES"
 sudo apt install $PACKAGES
 
 if [ "$(grep -c aka ~/.bashrc)" -eq "0" ]; then
-	echo "Setting up ~/.bashrc ..."
-	cat etc/bashrc_footer >> ~/.bashrc
+    echo "Setting up ~/.bashrc ..."
+    cat etc/bashrc_footer >> ~/.bashrc
 else
-	echo "Skipping setup for ~/.bashrc"
+    echo "Skipping setup for ~/.bashrc"
 fi
 
 cp etc/gitconfig ~/.gitconfig
 
 if [ -d ~/.bash-git-prompt ]; then
-	echo "Skipping setup of bash git prompt"
+    echo "Skipping setup of bash git prompt"
 else
-	git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
-	echo "Please setup bash git prompt themes by running git_prompt_make_custom_theme and editing ~/.git-prompt-colors.sh"
+    git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+    echo "Please setup bash git prompt themes by running git_prompt_make_custom_theme and editing ~/.git-prompt-colors.sh"
 fi
+
+#mkdir -p ~/.vim
+#rsync -a etc/vim/indent ~/.vim
+
+git clone https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+cd $CURR_DIR
