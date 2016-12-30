5f6c81766355e3fd39d54029b8e40fe005a25318
Cheng-Yu Pai <mail4charliepai@gmail.com>
2016-12-29 23:03:05 -0800

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Initializing temp directory..."
+mkdir -p temp
+rm -r temp/*
+
+PACKAGES="git vim xclip"
+echo "Installing packages: $PACKAGES"
+sudo apt install $PACKAGES
+
+if [ "$(grep -c aka ~/.bashrc)" -eq "0" ]; then
+	echo "Setting up ~/.bashrc ..."
+	wget -O temp/bashrc_footer https://gist.githubusercontent.com/cypai/d74f5de78fa13f6e20c19fc502691c85/raw/e6a3683da00e44359d70ce82649f3ec45dc4ae7d/.bashrc
+	cat temp/bashrc_footer >> ~/.bashrc
+	echo "" >> ~/.bashrc
+else
+	echo "Skipping setup for ~/.bashrc"
+fi
+
+if [ -d ~/.bash-git-prompt ]; then
+	echo "Skipping setup of bash git prompt"
+else
+	git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+	echo "Please setup bash git prompt themes by running git_prompt_make_custom_theme and editing ~/.git-prompt-colors.sh"
+fi
