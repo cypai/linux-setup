9aecb8c206ee20d9d4051d9284c6471dd7260797
Cheng-Yu Pai <mail4charliepai@gmail.com>
2017-04-02 19:36:32 -0700

    Add documentation for rust-related install

--- optional-setup.sh
+++ optional-setup.sh
@@ -3,5 +3,8 @@
 # Install rust
 curl https://sh.rustup.rs -sSf | sh
 
+# Add cargo to .bashrc by adding this line:
+# source $HOME/.cargo/env
+
 # Install rg
-cargo install ripgrep
+# cargo install ripgrep
