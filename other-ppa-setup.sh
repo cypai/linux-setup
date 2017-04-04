5849f0dfff7fe0a994c76cd31f3d395d8f4c51a5
Cheng-Yu Pai <mail4charliepai@gmail.com>
2017-04-03 21:49:50 -0700

    Additional ppas

--- other-ppa-setup.sh
+++ other-ppa-setup.sh
@@ -1,6 +1,7 @@
 sudo add-apt-repository ppa:nathan-renniewaldock/flux
 sudo add-apt-repository ppa:ubuntu-wine/ppa
 sudo add-apt-repository ppa:dawidd0811/neofetch
+sudo add-apt-repository ppa:mc3man/mpv-tests
 
 sudo apt update
-sudo apt install fluxgui wine1.8 neofetch
+sudo apt install fluxgui wine1.8 neofetch mono-complete mpv
