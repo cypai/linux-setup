4a45fc730d6141975136ee8488b0a3d5bc593f63
Cheng-Yu Pai <mail4charliepai@gmail.com>
2020-01-09 17:04:33 -0800

    Update macos setup

--- macos_setup.sh
+++ macos_setup.sh
@@ -12,7 +12,7 @@ mkdir temp
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew cask install java
-brew install git maven gradle ant wget pyenv direnv
+brew install git maven gradle ant wget pyenv direnv fzf
 brew cask install vlc
 
 cat <<EOF > ~/.bash_profile
