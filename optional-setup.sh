955072ea9179740a713be5d4032e5a423efb1f14
Cheng-Yu Pai <mail4charliepai@gmail.com>
2021-09-30 01:30:04 -0700

    Adjust rg install

--- optional-setup.sh
+++ optional-setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Install rust
-curl https://sh.rustup.rs -sSf | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Add cargo to .bashrc by adding this line:
-# source $HOME/.cargo/env
+source $HOME/.cargo/env
 
 # Install rg
-# cargo install ripgrep
+cargo install ripgrep
